a46bb06a0e432d5b0bae40380349efde14f01c68
Marcos Vieira De Souza <souzaaryadeivd@gmail.com>
2026-08-28 20:41:03 +0000

    Teste 03

--- Nicel Aventureiro/Escrita Manual 2.c
+++ Nicel Aventureiro/Escrita Manual 2.c
@@ -50,12 +50,6 @@ Densidade Populacional: 5622.24 hab/km²
 
 PIB per Capita: 44532.91 reais*/
 
-int main(void) {
-
-    char estado01[10], codigo01[10], cidade01[50];
-    char estado02[10], codigo02[10], cidade02[50];
-    float populacao01, area01, pib01, densidade01, pibPerCapita01;
-    float populacao02, area02, pib02, densidade02, pibPerCapita02;
-    int pontosTuristicos01, pontosTuristicos02;
+int main
 
   
