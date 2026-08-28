fa593de69ba3e1ccd7a00b9add4900f83bc38338
Marcos Vieira De Souza <souzaaryadeivd@gmail.com>
2026-08-28 16:20:52 -0400

    Update Escrita Manual 2.c

--- Nicel Aventureiro/Escrita Manual 2.c
+++ Nicel Aventureiro/Escrita Manual 2.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-int main (){
-
 /*Carta Numero 01*/
   char Estado01[10];
   char Código01[10];
@@ -22,6 +20,38 @@ int main (){
   int NumerodePontosTuristicos02;
   float DensidadePopulacional02;
   float PIBperCapita02;
+
+int main() {
+
+printf ("Informe o seu Estado01:\n");
+scanf ("%s", Estado01);
+
+printf ("Informe o Codigo01:\n");
+scanf ("%s", Codigo01);
+
+printf ("Informe o NomedaCidade01:\n");
+scanf ("%s", NomedaCidade01);
+
+printf ("Informe a População01:\n");
+scanf ("%f", &População01);
+
+printf ("Informe a Area01:\n");
+scanf ("%f", &Area01);
+
+printf ("Informe o PIB01:\n");
+scanf ("%f", &PIB01);
+
+printf ("Informe o Número de Pontos Turísticos01:\n");
+scanf ("%d", &NúmeroDePontosTurísticos01);
+
+printf ("Informe a Densidade Populacional01:\n");
+scanf ("%f, &Densidadepopulacional01);
+
+printf ("Informe a PIB per Capita01:\n");
+scanf ("%f, &PIBperCapita01);
+
+
+
 
 
 
