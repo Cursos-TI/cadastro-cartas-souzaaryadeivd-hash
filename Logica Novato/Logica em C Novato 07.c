c6f284bb5f434a21ac731ad8422889b8f2f86b3e
Marcos Vieira De Souza <souzaaryadeivd@gmail.com>
2026-09-08 14:18:26 -0400

    Update Logica em C Novato 07.c

--- Logica Novato/Logica em C Novato 07.c
+++ Logica Novato/Logica em C Novato 07.c
@@ -1,1 +1,5 @@
-
+#include <stdio.h>
+int main () {
+/* if (condicao) {//bloco de codigo a ser executado se a condição for verdadeira//}
+else {//bloco de codigo a ser executado se a condição for falsa//}*/
+}
